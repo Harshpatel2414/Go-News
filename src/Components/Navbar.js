863d4b5d7066181eae7eb366970cc24933bf5c88
Harshal Patel <harshalpateljnv@gmail.com>
2023-08-20 22:00:10 +0530

    add infinity scroll and fix image loading bug

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div>
             <nav className="navbar fixed-top  navbar-expand-lg navbar-dark bg-dark ">
-                <div className="container-fluid " >
+                <div className="container-fluid " style={{'overflow':'hidden'}}>
                     <Link className="navbar-brand" to="/">Go-News</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,56 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import NewsItems from './NewsItems';
 import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const News = (props) => {
     const [results, setResults] = useState([])
     const [page, setPage] = useState(1)
-    const [totalResults, setTotalResults] = useState(0)
+    const [totalResults, setTotalResults] = useState(0)    
 
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
+    
     const newsUpdate = async ()=> {
-        // const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-        const url =`https://newsdata.io/api/1/news?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&language=en`
+        const url =`https://newsdata.io/api/1/news?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&language=en&image=1&size=${props.pageSize}`
         let data =  await fetch(url);
         let parsedata = await data.json();
         setResults(parsedata.results)
         setTotalResults(parsedata.totalResults)
     }
-    
+
     useEffect(() => {
         document.title = `GO-News - ${capitalizeFirstLetter(props.category)} top headlines`;
         newsUpdate();
         // eslint-disable-next-line
     }, []);
-    
-    const showNext = async () => {
-        setPage(page + 1);
-        newsUpdate();
-    }
 
-    const showPrev = async () => {
-        setPage(page - 1);
-        newsUpdate();
-    }
+    const fetchData = async () => {   
+        const url = `https://newsdata.io/api/1/news?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&language=en&image=1&size=${props.pageSize}`;
+        setPage(page+1);
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        setResults(results.concat(parsedData.results));
+        setTotalResults(parsedData.totalResults);
+    };
 
     return (
         <div className='container my-75 ' style={{ "margin": "75px auto" }}>
-            <h2 className='text-center text-success'>Today's latest Top Headlines</h2>
+            <h2 className='text-center text-dark'>Today's latest Top Headlines</h2>
+            <InfiniteScroll
+                    dataLength={results.length}
+                    next={fetchData}
+                    hasMore={results.length !== totalResults}
+                    loader={<p>Loading...</p>}
+                > 
             <div className="row">
-                {results.map((element) => {
+                
+                {results && results.map((element) => {
                     return <div className='col-md-4' key={element.link}>
                         <NewsItems title={element.title ? element.title : ""} content={element.content ? element.content : ""}
-                            link={element.link} imageUrl={element.image_url} author={element.author} date={element.pubDate} nextPage={element.nextPage} />
+                            link={element.link} imageUrl={element.image_url} author={element.author} date={element.pubDate} />
                     </div>
                 })
                 }
             </div>
-            <div className='container d-flex justify-content-between'>
-                <button disabled={page <= 1} type='button' className='btn btn-dark' onClick={showPrev}>&larr; Previous</button>
-                <button disabled={page + 1 > Math.ceil(totalResults / props.pageSize)} type='button' className='btn btn-dark' onClick={showNext}>Next &rarr;</button>
-            </div>
+           </InfiniteScroll>
         </div>
     )
 }
